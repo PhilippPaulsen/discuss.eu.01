fac5a061f7119c882a8140a5f5044b957c8a16db
Philipp Paulsen <paulsen@phlip.site>
2025-03-01 17:32:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Discurs.eu", page_icon="💬", layout="wide")
 
 # Custom App Title (instead of "Streamlit app")
-st.markdown("<h1 style='text-align: center;'>💬 Discurs.eu</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: left;'>💬 Discurs.eu</h1>", unsafe_allow_html=True)
 
 st.write("""
 **Welcome to Discurs.eu!**
