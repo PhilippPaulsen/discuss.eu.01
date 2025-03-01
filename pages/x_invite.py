ff12407221e1c59705a4dd6d029226ce2e58422f
Philipp Paulsen <paulsen@phlip.site>
2025-03-01 18:31:03 +0100

    Update x_invite.py

--- pages/x_invite.py
+++ pages/x_invite.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from utils import apply_custom_styles  # Import the styling function
 
-st.title("📩 Invite Users")
+st.set_page_config(page_title="Discurs.eu", page_icon="💬", layout="wide")
+
+# Custom Title
+st.markdown("""
+    <h1 style='text-align: left; font-family: "Suisse", sans-serif; font-weight: bold;'>
+        📩 Invite Users
+    </h1>
+""", unsafe_allow_html=True)
+
 st.write("Send an invitation to new users to join a discussion on Discurs.eu.")
 
 email = st.text_input("Enter Email Address")
