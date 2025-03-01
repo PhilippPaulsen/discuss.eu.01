4d7701e694f02574d8de2dcfbbf404a7990b421c
Philipp Paulsen <paulsen@phlip.site>
2025-03-01 18:24:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,17 @@
 import streamlit as st
+from utils import apply_custom_styles  # Import the styling function
 
-# Set the page title & remove "Streamlit app" from navigation
 st.set_page_config(page_title="Discurs.eu", page_icon="💬", layout="wide")
 
-# Custom Styling for Suisse Typeface
-st.markdown("""
-    <style>
-        @font-face {
-            font-family: 'Suisse';
-            src: url('/static/assets/SuisseIntl-Regular.woff2') format('woff2');
-            font-weight: normal;
-            font-style: normal;
-        }
-        
-        @font-face {
-            font-family: 'Suisse';
-            src: url('/static/assets/SuisseIntl-Bold.woff2') format('woff2');
-            font-weight: bold;
-            font-style: normal;
-        }
-        
-        html, body, [class*="st-"] {
-            font-family: 'Suisse', sans-serif;
-        }
-        
-        h1, h2, h3, h4, h5, h6 {
-            font-family: 'Suisse', sans-serif;
-            font-weight: bold;
-        }
-    </style>
-""", unsafe_allow_html=True)
+# Apply global styles
+apply_custom_styles()
 
-# Custom App Title (instead of "Streamlit app")
+# Custom App Title
 st.markdown("""
     <h1 style='text-align: left; font-family: "Suisse", sans-serif; font-weight: bold;'>
         💬 Discurs.eu
     </h1>
 """, unsafe_allow_html=True)
-
 
 st.write("""
 **Welcome to Discurs.eu!**
