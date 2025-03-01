9b29b173c8f128e0e40496f7e746ab794ce518c9
Philipp Paulsen <paulsen@phlip.site>
2025-03-01 18:35:57 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,13 +17,15 @@ def apply_custom_styles():
                 font-style: normal;
             }
 
-            html, body, [class*="st-"] {
-                font-family: 'Suisse', sans-serif;
+            html, body, [class*="st-"], h1, h2, h3, h4, h5, h6 {
+                font-family: 'Suisse', sans-serif !important;
             }
 
-            h1, h2, h3, h4, h5, h6 {
-                font-family: 'Suisse', sans-serif;
-                font-weight: bold;
+            h1 {
+                font-weight: 700;
+            }
+            h2, h3, h4, h5, h6 {
+                font-weight: 600;
             }
         </style>
     """, unsafe_allow_html=True)
