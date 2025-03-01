c1d9c0d2eec8338a2b906e28bac890932dec271c
Philipp Paulsen <paulsen@phlip.site>
2025-03-01 18:32:02 +0100

    Update x_sign_in.py

--- pages/x_sign_in.py
+++ pages/x_sign_in.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from utils import apply_custom_styles  # Import the styling function
 
-st.title("🔑 Sign In to Discurs.eu")
+st.set_page_config(page_title="Discurs.eu", page_icon="💬", layout="wide")
+
+# Custom Title
+st.markdown("""
+    <h1 style='text-align: left; font-family: "Suisse", sans-serif; font-weight: bold;'>
+        🔑 Sign In to Discurs.eu
+    </h1>
+""", unsafe_allow_html=True)
 
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
