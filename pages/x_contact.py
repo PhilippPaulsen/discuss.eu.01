271507ac4b312e25e1d90dac80d2fa1602e3a115
Philipp Paulsen <paulsen@phlip.site>
2025-03-01 18:30:31 +0100

    Update x_contact.py

--- pages/x_contact.py
+++ pages/x_contact.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from utils import apply_custom_styles  # Import the styling function
 
-st.title("📞 Contact & Support")
+st.set_page_config(page_title="Discurs.eu", page_icon="💬", layout="wide")
+
+# Custom Title
+st.markdown("""
+    <h1 style='text-align: left; font-family: "Suisse", sans-serif; font-weight: bold;'>
+        📞 Contact & Support
+    </h1>
+""", unsafe_allow_html=True)
 
 st.write("For support, feedback, or inquiries, please reach out to us.")
 
